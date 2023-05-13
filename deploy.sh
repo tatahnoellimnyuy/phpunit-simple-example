165bf59c08309ff476e19165ac2cc65fcb6f5c70
tatahnoellimnyuy <tatahnoellimnyuy@gmail.com>
2023-05-13 23:53:41 +0200

    added pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "shibang"
+# FTP server details
+HOST="ftp.example.com"
+USERNAME="toxic"
+PASSWORD="Jesuisroot123@"
+
+# Directory on the FTP server to upload the code to
+REMOTE_DIR="~/"
+
+# Local directory containing the PHP code to be deployed
+LOCAL_DIR="./"
+
+# FTP deployment using lftp
+lftp -c "open -u $USERNAME,$PASSWORD $HOST; mirror --reverse --delete --verbose $LOCAL_DIR $REMOTE_DIR"
